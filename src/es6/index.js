eb30f6cdf8c0415b5475f8b1ef1f2293eb15a893
Jose Cuevas <josecueram@gmail.com>
2020-04-08 20:55:45 -0500

    added multiline strings example

--- src/es6/index.js
+++ src/es6/index.js
@@ -12,3 +12,9 @@ const petAge = 5
 const petType = 'cat'
 const phrase = `Hello my ${petType}'s name is ${petName}, it is ${petAge} years old`
 console.log(phrase)
+
+// * Multiline strings
+const song = `Eleanor Rigby, picks up the rice in a church where a wedding has been,
+lives in a dream.
+`
+console.log(song)
